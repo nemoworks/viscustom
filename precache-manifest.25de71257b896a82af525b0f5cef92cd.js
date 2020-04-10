90cd20ae0aeaea98f68e01f63207b85a979284e7
perishcode@gmail.com <perishcode@gmail.com>
2020-04-10 15:34:37 +0800

    first build

---
+++ precache-manifest.25de71257b896a82af525b0f5cef92cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c90e2574f5fb7f02a9812d973dec40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e6518bd98945298e09d",
+    "url": "/static/css/main.f0b727f0.chunk.css"
+  },
+  {
+    "revision": "04706ae8007d9a118a7d",
+    "url": "/static/js/2.2373cc18.chunk.js"
+  },
+  {
+    "revision": "5d47eebbc14270e80e7729ca5f98b80c",
+    "url": "/static/js/2.2373cc18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e6518bd98945298e09d",
+    "url": "/static/js/main.cec2feff.chunk.js"
+  },
+  {
+    "revision": "b2508b4b72c8a1384917",
+    "url": "/static/js/runtime-main.d9a735b8.js"
+  }
+]);
