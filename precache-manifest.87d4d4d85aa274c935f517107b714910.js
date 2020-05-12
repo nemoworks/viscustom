2e3b97b61a6145d42f4875606e86a44341582360
PerishCode <perishcode@gmail.com>
2020-05-12 11:58:57 +0800

    Updates

--- precache-manifest.87d4d4d85aa274c935f517107b714910.js
+++ precache-manifest.87d4d4d85aa274c935f517107b714910.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4e85fe916897df35166db1d1a6ef7e46",
+    "revision": "f43e3cf350e4dd7d5d128c0422d27394",
     "url": "/viscustom/index.html"
   },
   {
-    "revision": "49ec2c3f798361ab089e",
+    "revision": "325581cf58765294b6cc",
     "url": "/viscustom/static/css/main.f0b727f0.chunk.css"
   },
   {
-    "revision": "666da50f6aeeeac534c7",
-    "url": "/viscustom/static/js/2.b1a906a0.chunk.js"
+    "revision": "820c36a455ad79c8b4ad",
+    "url": "/viscustom/static/js/2.b8f35313.chunk.js"
   },
   {
     "revision": "5d47eebbc14270e80e7729ca5f98b80c",
-    "url": "/viscustom/static/js/2.b1a906a0.chunk.js.LICENSE.txt"
+    "url": "/viscustom/static/js/2.b8f35313.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "49ec2c3f798361ab089e",
-    "url": "/viscustom/static/js/main.b7a1a877.chunk.js"
+    "revision": "325581cf58765294b6cc",
+    "url": "/viscustom/static/js/main.9398a48f.chunk.js"
   },
   {
     "revision": "bf628dbf5159a9b53b4e",
