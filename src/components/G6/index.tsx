23c07f97d8ae493907a80c8ebdc93fd63d8890a2
PerishCode <perishcode@gmail.com>
2020-05-14 15:46:26 +0800

    algorithm update

--- src/components/G6/index.tsx
+++ src/components/G6/index.tsx
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom'
 import G6 from '@antv/g6'
 import initialEdges from './edges.json'
 import initialNodes from './nodes.json'
-import { findPath } from '../../utils/path'
+import { findPaths } from '../../utils/path'
 
 let graph: any = null
+
+const centerId = '2500631037'
 
 const nodes = initialNodes.map((node) => ({
   id: node.custNo,
   name: node.name,
   style: {
-    fill: node.custNo === '2500631037' ? '#FF00FF' : '#00FFFF',
+    fill: node.custNo === centerId ? '#FF00FF' : '#00FFFF',
   },
 }))
 
@@ -26,76 +28,83 @@ const edgeList = initialEdges.map((edge) => ({
 }))
 
 export default function () {
-  const ref = React.useRef(null)
-
   const onNodeClick = (e: any) => {
     const id = e.item._cfg.id
-    if (id === '2500631037') return
-    // const model = graph.findById(id)
+    if (id === centerId) return
 
     const model = graph.findById(id)
 
     const { selected } = model._cfg.model
 
-    findPath(edgeList, '2500631037', id, (path: String[]) => {
-      path.slice(1, -1).forEach((id) => {
-        const model = graph.findById(id)
-        graph.update(model, {
-          label: selected ? '' : id,
-          style: {
-            fill: selected ? '#00FFFF' : '#112233',
-          },
-        })
+    const weight = findPaths(edgeList, centerId, id, (path: String[]) => {})
+
+    let max = 0.0
+
+    for (const key in weight) if (weight[key] > max) max = weight[key]
+
+    for (const key in weight) {
+      if (key === centerId || key === id) continue
+      const gray = Math.round(255 * (1 - weight[key] / max))
+      graph.update(key, {
+        style: {
+          fill: selected ? '#00FFFF' : `rgb(${gray},${gray},${gray})`,
+        },
+        label: selected ? '' : weight[key],
       })
+    }
+
+    graph.getNodes().forEach((node: any) => {
+      const nodeId = node._cfg.id
+
+      if (nodeId !== centerId && nodeId !== id && !weight[nodeId]) {
+        selected ? graph.showItem(nodeId) : graph.hideItem(nodeId)
+      }
     })
 
     graph.update(model, {
       ...model,
       style: {
-        fill: selected ? '#00FFFF' : '#FF3388',
+        fill: selected ? '#00FFFF' : '#FF3322',
       },
       selected: !selected,
     })
   }
 
+  const ref = React.useRef(null)
+
   useEffect(() => {
-    if (!graph) {
-      graph = new G6.Graph({
-        container: ReactDOM.findDOMNode(ref.current) as HTMLElement,
-        width: 1100,
-        height: 800,
-        modes: {
-          default: ['drag-canvas', 'zoom-canvas'], // 允许拖拽画布、放缩画布、拖拽节点
-        },
-        layout: {
-          type: 'force',
-          preventOverlap: true,
-          linkDistance: 80,
-          nodeSize: 20,
-        },
-        defaultNode: {
-          type: 'node',
-          labelCfg: {
-            style: {
-              fill: '#000000A6',
-              fontSize: 10,
-            },
-          },
-          size: 10,
+    graph = new G6.Graph({
+      container: ReactDOM.findDOMNode(ref.current) as HTMLElement,
+      width: 1100,
+      height: 800,
+      modes: {
+        default: ['drag-canvas', 'zoom-canvas'], // 允许拖拽画布、放缩画布、拖拽节点
+      },
+      layout: {
+        type: 'force',
+        preventOverlap: true,
+        linkDistance: 80,
+        nodeSize: 20,
+      },
+      defaultNode: {
+        type: 'node',
+        labelCfg: {
           style: {
-            fill: '#00FFFF',
-            stroke: '#002222',
+            fill: '#000000A6',
+            fontSize: 10,
           },
         },
-      })
-
-      graph.on('node:click', onNodeClick)
-
-      graph.data({ nodes, edges })
-    }
-
+        size: 10,
+        style: {
+          fill: '#00FFFF',
+          stroke: '#002222',
+        },
+      },
+    })
+    graph.on('node:click', onNodeClick)
+    graph.data({ nodes, edges })
     graph.render()
-  })
+  }, [])
 
   return <div ref={ref} style={{ overflow: 'auto' }} />
 }
--- src/utils/path.ts
+++ src/utils/path.ts
@@ -3,35 +3,126 @@ interface Edge {
   target: string
 }
 
-export function findPath(
+interface EdgeList {
+  [key: string]: Set<string>
+}
+
+function findPath(
+  edgeList: EdgeList,
+  from: string,
+  to: string,
+  callback: any,
+  path: String[],
+  depth: number = 0
+) {
+  if (path.find((node) => node === from) || depth > 8) return
+
+  if (edgeList[from]) {
+    path.push(from)
+    for (const target of edgeList[from]) {
+      if (target === to) {
+        path.push(target)
+        callback(path)
+        path.pop()
+        break
+      } else findPath(edgeList, target, to, callback, path, depth + 1)
+    }
+  }
+
+  if (path.length > 0) path.pop()
+}
+
+export function _findPath(
   edges: Edge[],
-  source: string,
-  target: string,
+  from: string,
+  to: string,
   callback: any = (path?: String[]) => {},
-  path: String[] = []
+  path: String[] = [],
+  depth: number = 0
 ) {
   // 遍历过程中存在环，直接结束本次遍历过程
-  if (path.find((node) => node === source)) {
+  if (path.find((node) => node === from) || depth > 8) {
     path.pop()
     return
   }
 
   for (const edge of edges) {
-    if (edge.source === source) {
-      path.push(source)
-      if (edge.target === target) {
-        // 发现路径 调用回调函数
-        path.push(target)
+    let { source, target } = edge
+
+    if (source !== from) {
+      let temp = source
+      source = target
+      target = temp
+    }
+
+    if (source === from) {
+      debugger
+      path.push(from)
+      if (target === to) {
+        path.push(to)
         callback(path)
         path.pop()
         path.pop()
         continue
       }
-      // 递归遍历
-      findPath(edges, edge.target, target, callback, path)
+      _findPath(edges, target, to, callback, path, depth + 1)
+    }
+  }
+  if (path.length > 0) path.pop()
+}
+
+export function findPaths(
+  edges: Edge[],
+  from: string,
+  to: string,
+  callback: any
+) {
+  //初始化邻接集合
+  const edgeList: EdgeList = {}
+
+  edges.forEach(({ source, target }) => {
+    if (edgeList[source] === undefined) edgeList[source] = new Set()
+    if (edgeList[target] === undefined) edgeList[target] = new Set()
+    edgeList[source].add(target)
+    edgeList[target].add(source)
+  })
+
+  const pathStack = [from]
+  const sideStack = [Array.from(edgeList[from])]
+  const weight: {
+    [key: string]: number
+  } = {}
+
+  // debugger
+
+  while (pathStack.length > 0) {
+    let nodeList = sideStack.pop() || []
+    if (nodeList.length > 0) {
+      let node = nodeList.pop() as string
+      pathStack.push(node)
+      sideStack.push(nodeList)
+      if (edgeList[node]) {
+        let temp = edgeList[node]
+
+        let item = Array.from(temp).filter(
+          (target) => !pathStack.find((n) => n === target)
+        )
+        sideStack.push(item)
+      }
+    } else pathStack.pop()
+
+    if (pathStack.length > 0 && pathStack[pathStack.length - 1] === to) {
+      // debugger
+
+      pathStack.forEach((n) => {
+        if (weight[n] === undefined) weight[n] = 0
+        weight[n]++
+      })
+      // callback(Array.from(pathStack))
+      pathStack.pop()
+      sideStack.pop()
     }
   }
 
-  // 不存在匹配路径，弹出栈顶节点
-  if (path.length > 0) path.pop()
+  return weight
 }
